6371e3560a3fa57e3f0c5836bb4138aebbe5878c
rtem92 <rtem92@gmail.com>
2017-07-10 12:13:04 +0300

    Updated SQL queries + added categories endpoint

---
+++ models/Category.js
@@ -0,0 +1,31 @@
+var db = require('../dbconnection');
+
+var Category = {
+    getAllCategories: function(callback) {
+        return db.query('select * from categories', callback);
+    },
+    
+    getCategoryById: function(id, callback) {
+        return db.query('select * from categories where id=?', [id], callback);
+    },
+    
+    addCategory: function(User, callback) {
+        return db.query('insert into categories values(?,?)', [Category.id, Category.name, Category.description], callback);
+    },
+    
+    deleteCategory:function(id, callback) {
+        return db.query('delete from categories where id=?', [id], callback);
+    },
+
+    updateCategory: function(id, User, callback) {
+        return  db.query('update categories set name=?, description=?, where id=?', [Category.name, Category.description, id],callback);
+    },
+    
+    deleteAllCategories: function(item, callback) {
+        var delarr = [];
+        for(i = 0; i < item.length; i++) {
+            delarr[i] = item[i].Id;
+        }
+        return db.query('delete from categories where id in (?)', [delarr], callback);
+    }
+}
