3331a7ca10003c3dce2e8fb78390a5bbad083c77
rtem92 <rtem92@gmail.com>
2017-07-17 20:37:26 +0300

    Fix auth endpoint

--- models/Auth.js
+++ models/Auth.js
@@ -2,6 +2,8 @@ var db = require('../dbconnection');
 
 var Auth = {
     login: function(email, password, callback) {
-        return db.query('select * from users where email=?, passwrod=?', email, password, callback);
+        return db.query('select * from users where email=? and password=?', [email, password], callback);
     },
 }
+
+module.exports = Auth;
